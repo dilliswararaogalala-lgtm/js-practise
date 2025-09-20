f34f86c0dd5e4863e7874a2844e4bbcecb0a4210
dilliswararaogalala-lgtm <dilliswararaogalala@gmail.com>
2025-09-20 13:07:03 +0530

    Update to for loop to find space between Zebra and Lion vice versa

--- lion_hunt/lion_hunt.js
+++ lion_hunt/lion_hunt.js
@@ -1,38 +1,20 @@
-const testCase= "L      Z"
+const testCase = "Z  L"
 let emptyLand = 0;
-let index  = 0;
+let index = 0;
 let length = testCase.length;
-if(testCase[index] === "L" ){
-    index = index+1;
-    if(testCase[index] !== "Z" && index <= length){
-        emptyLand++;
-    }
-    index = index+1;
-    
-    if(testCase[index] !== "Z" && index <= length){
-        emptyLand++;
-    }
-    index = index+1;
-    
-    if(testCase[index] !== "Z" && index <= length){
-        emptyLand++;
-    }
-    index = index+1;
-    
-    if(testCase[index] !== "Z" && index <= length){
-        emptyLand++;
-    } 
-    index = index+1;
-    
-    if(testCase[index] !== "Z" && index <= length){
-        emptyLand++;
-    }
-    index = index + 1;
 
-    if(testCase[index] !== "Z" && index <= length){
-        emptyLand++;
+if(testCase[0] === "L"){
+    for(let index = 1; index <= length - 1; index++ ){
+        if(testCase[index] !== "Z"){
+            emptyLand++;
+        }
     }
-    index = index + 1;
-    
+}else if(testCase[0] === "Z"){
+   for(let index = 1; index <= length - 1; index++ ){
+        if(testCase[index] !== "L"){
+            emptyLand++;
+        }
+    } 
 }
-console.log("empty patch between lion and zebra :",emptyLand);
+
+console.log("empty patch between lion and zebra or Zebra and lion :", emptyLand);
