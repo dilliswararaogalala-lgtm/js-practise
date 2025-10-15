fe3e09601fc70f97026c68b5a12145a42c6e5425
dilliswararaogalala-lgtm <dilliswararaogalala@gmail.com>
2025-10-15 11:57:05 +0530

    Add one more function to play until the chances are done

--- Developing programs/cowsAndBulls.js
+++ Developing programs/cowsAndBulls.js
@@ -2,41 +2,52 @@ function isHeWon(cows, bulls, number) {
   if (bulls === number.length && cows === 0) {
     console.log();
     console.log("YOU WON 🏆");
-    return;
+    return true;
   } else {
     console.log();
     console.log(`🐮 cows count: ${cows} \n🐂 bulls count: ${bulls}`);
-    return;
+    return false;
   }
 }
 
-function startGame(secretNumber, guessedNumber, cows, bulls) {
-  for (let index = 0; index < guessedNumber.length; index++) {
-    if (secretNumber.includes(guessedNumber[index])) {
-      const indexOfNumber = (guessedNumber.indexOf(guessedNumber[index]));
-      if (secretNumber[indexOfNumber] === guessedNumber[index]) {
-        bulls++;
-      } else {
-        cows++;
+function cowsAndBullsGame(secretNumber, chances, cows, bulls) {
+  let isWon = false;
+  while (!isWon && chances > 0) {
+    const guessedNumber = prompt('enter your  number:').split('');
+
+    for (let index = 0; index < guessedNumber.length; index++) {
+      if (secretNumber.includes(guessedNumber[index])) {
+        const indexOfNumber = (guessedNumber.indexOf(guessedNumber[index]));
+        if (secretNumber[indexOfNumber] === guessedNumber[index]) {
+          bulls++;
+        } else {
+          cows++;
+        }
       }
     }
+
+    isWon = isHeWon(cows, bulls, secretNumber);
+    cows = 0; bulls = 0;
+    chances--;
   }
-  return isHeWon(cows, bulls, secretNumber);
+  console.log(`secret number is : ${secretNumber.join('')}`);
 }
 
-function getRandomNumber() {
-  const num = [];
-  for (let index = 0; index < 4; index++) {
-    const numberAsString = '' + (Math.floor(Math.random() * 9 + 1));
-    num.push(numberAsString);
+function getRandomNumber(length) {
+  let num = '';
+  while (length > 0) {
+    const number = '' + Math.floor(Math.random() * 9 + 1);
+    if (num.includes(number)) continue;
+    num += number;
+    length--;
   }
-  return num;
+  return num.split('');
 }
 
 function main() {
-  const secretNumber = getRandomNumber();
-  const guessedNumber = prompt('enter your  number:').split('');
-  startGame(secretNumber, guessedNumber, 0, 0);
+  const lengthOfNumber = prompt("enter the length of number: ")
+  const secretNumber = getRandomNumber(lengthOfNumber);
+  cowsAndBullsGame(secretNumber, 5, 0, 0);
 }
 
 main();
