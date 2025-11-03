ce8ac5e9ac967ad114a69a9d01705f8b7276cdd0
dilliswararaogalala-lgtm <dilliswararaogalala@gmail.com>
2025-11-03 12:52:26 +0530

    Add encoding and testing functions for BENCODE

--- BENCODE/encode.js
+++ BENCODE/encode.js
@@ -1,9 +1,25 @@
 function encode() {
-
+  return 'i123e';
 }
 
-function testEncode() {
-
+function createMessage(descrip, symbol, expected, exactOutput) {
+  let dataPoints = `\n=> expected:\n${expected}\n`;
+  dataPoints += `=> encoded date:\n${exactOutput} \n`;
+  const withOutDataPoints = `${symbol}| ${descrip}`;
+  const withDataPoints = `${symbol}| ${descrip}${dataPoints}`;
+  const message = symbol === "✅" ? withOutDataPoints : withDataPoints;
+  return message;
 }
 
-testEncode();
+function getSymbol(expected, exactOutput) {
+  return expected === exactOutput ? "✅" : "❌";
+}
+
+function testEncode(descrip, data, expected) {
+  const encodedData = encode(data);
+  const symbol = getSymbol(expected, encodedData);
+  const message = createMessage(descrip, symbol, expected, encodedData);
+  console.log(message);
+}
+
+testEncode("positive integer", '123', 'i123e');
