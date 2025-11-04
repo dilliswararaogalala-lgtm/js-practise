c6484f57a5f36723b9c6e2841feb66dbc578dc81
dilliswararaogalala-lgtm <dilliswararaogalala@gmail.com>
2025-11-04 12:33:08 +0530

    Enhance sorting.js with random data generation
    Added functions to generate random data and count swaps.

--- sorting.js
+++ sorting.js
@@ -1,6 +1,9 @@
+let numberOfTimes = 0;
+
 function sort(data) {
   for (let i = 0; i < data.length; i++) {
     for (let j = i + 1; j < data.length; j++) {
+      numberOfTimes++;
       if (data[i] < data[j]) {
         const temp = data[j];
         data[j] = data[i];
@@ -11,9 +14,24 @@ function sort(data) {
   return data;
 }
 
-const data = [5, 10, 22, 10];
+function getRandomNumber(lowerValue, upperValue) {
+  return lowerValue + Math.floor(Math.random() * upperValue - lowerValue);
+}
+
+function getRandomData(numberOfElements) {
+  const data = [];
+  for (let index = 0; index < numberOfElements; index++) {
+    const randomNumber = getRandomNumber(1, 100);
+    data.push(randomNumber);
+  }
+  return data;
+}
+
+const data = getRandomData(10);
 const sortedData = sort(data);
 
 for (let index = 0; index < sortedData.length; index++) {
   console.log(sortedData[index]);
 }
+
+console.log(`number of times:${numberOfTimes}`);
