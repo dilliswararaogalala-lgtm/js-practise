40cd766596afdfe986f089d5e2414dafd610f560
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-12 05:56:27 +0530

    solved two questions

---
+++ generators_and_iterators/generators.js
@@ -0,0 +1,28 @@
+// - Generate all pair permutations
+//   [1,2,3,4,5] => [[1,2],[1,3],[1,4],[1,5],[2,3],[2,4],[2,5],...]
+
+const permutations = function* (data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      yield [data[i], data[j]];
+    }
+  }
+};
+
+const iterator = permutations([1,2,3,4,5]);
+console.log([...iterator]);
+
+// Generate sequences of consecutive pairs
+//   [1,2,3,4,5] => [[1,2],[2,3],[3,4],[4,5]]
+
+const consecutivePair = function* (data) {
+  for (let index = 0; index < data.length; index++) {
+    yield [data[index], data[index] + 1];    
+  }
+}
+
+const ite = consecutivePair([1,2,3,4,5]);
+console.log([...ite]);
+
+// Generate a cycle of elements
+//   [1,2,3,4,5] => [1,2,3,4,5,1,2,3,4,5,...]
