33746ea8eda149fd5aef1e95db67ee101f3aba4c
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-13 14:29:25 +0530

    solved prime number series

---
+++ generators_and_iterators/primeSequence.js
@@ -0,0 +1,18 @@
+// - Prime number series
+
+const isPrime = (x) => {
+  const isDivisible = (x, y) => x % y === 0;
+  if (x === 1) return false;
+  for (let i = 2; i < x; i++) {
+    if (isDivisible(x, i)) return false;
+  }
+  return true;
+};
+
+const primeNumberSeries = function* () {
+  let i = 1;
+  while (true) {
+    if (isPrime(i)) yield i;
+    i++;
+  }
+};
