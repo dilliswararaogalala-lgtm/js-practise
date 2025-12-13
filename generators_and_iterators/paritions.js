0acfe8650d2e4974425cd433fe04473c7018870a
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-13 14:08:55 +0530

    chnaged my partitionscode

--- generators_and_iterators/paritions.js
+++ generators_and_iterators/paritions.js
@@ -1,10 +1,11 @@
 // partition by
 //   identity: [1,1,1,2,2,1,1,3,3,2] => [[1,1,1],[2,2],[1,1],[3,3],[2]]
 //   isEven: [1,3,1,2,2,1,1,3,5,2] => [[1,3,1],[2,2],[1,1,3,5],[2]]
-const partition1 = function* (data) {
+
+const partition = function* (data, shouldBeGroup) {
   let partition = [data[0]];
   for (let index = 0; index < data.length; index++) {
-    if (partition.includes(data[index + 1])) {
+    if (shouldBeGroup(data[index], data[index + 1])) {
       partition.push(data[index + 1]);
     } else {
       yield partition;
@@ -13,28 +14,10 @@ const partition1 = function* (data) {
   }
 };
 
-const isEven = (x) => x % 2 === 0;
-const isOdd = (x) => x % 2 !== 0;
+const sameIdentity = (a, b) => a === b;
+const sameGroup = (a, b) => (a % 2) === (b % 2);
 
-const isBothSame = (x, y, predicate) => predicate(x) && predicate(y);
-
-const partition2 = function* (data) {
-  let partition = [data[0]];
-  for (let index = 0; index < data.length; index++) {
-    if (
-      isBothSame(data[index], data[index + 1], isEven) ||
-      isBothSame(data[index], data[index + 1], isOdd)
-    ) {
-      partition.push(data[index + 1]);
-    } else {
-      yield partition;
-      partition = [data[index + 1]];
-    }
-  }
-};
-
-const iterat = partition2([1, 3, 1, 2, 2, 1, 1, 3, 5, 2]);
-const iter = partition1([1, 1, 1, 2, 2, 1, 1, 3, 3, 2]);
-
-console.log([...iterat])
-console.log([...iter])
+const iterator = partition([1, 3, 1, 2, 2, 1, 1, 3, 5, 2], sameGroup);
+const iterator1 = partition([1, 1, 1, 2, 2, 1, 1, 3, 3, 2], sameIdentity);
+console.log([...iterator]);
+console.log([...iterator1]);
